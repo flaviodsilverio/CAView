3fdab1ed9aed1b0bd1a08e7b7dcb1f48d1101540
flaviodsilverio <flaviosilverio@MBP-de-Flavio.lan>
2015-08-02 10:37:35 +0100

    Small Tweaks and added a devo View Controller

--- CAView/CAView.swift
+++ CAView/CAView.swift
@@ -9,213 +9,213 @@
 import UIKit
 
 class CAView: UIView {
+    
+    enum StartingPoint{
         
-        enum StartingPoint{
-            
-            case Top
-            case Right
-            case Bottom
-            case Left
-            
-        }
-        
-        var startingPoint : StartingPoint = StartingPoint.Top
-        
-        private var backgroundCircle : CAShapeLayer = CAShapeLayer()
-        private let animatedCircle   : CAShapeLayer = CAShapeLayer()
-        
-        internal var backgroundCircleColor : UIColor = UIColor.grayColor()
-        internal var fillColor : UIColor = UIColor.blueColor()
-        
-        internal var clockwise : Bool = Bool(true)
-        internal var percentage : CGFloat = 1.0
-        private  var auxNumber : CGFloat = 0
-        
-        private var label : UILabel = UILabel()
-        private var timer : NSTimer = NSTimer()
-        private var userText : Bool = Bool(false)
-        
-        override init(frame: CGRect){
-            
-            super.init(frame: frame)
-            
-            self.backgroundColor = UIColor.clearColor()
-            self.layer.cornerRadius = self.frame.height / 2
-            self.clipsToBounds = true
-            
-            configureTextLabel("100%")
-        }
-        
-        
-        init(frame: CGRect, backGroundColor: UIColor, text: String, andFillColor: UIColor) {
-            
-            super.init(frame: frame)
-            
-            self.backgroundColor = UIColor.clearColor()
-            self.layer.cornerRadius = self.frame.height / 2
-            self.clipsToBounds = true
-            
-            self.backgroundCircleColor = backGroundColor
-            self.fillColor = andFillColor;
-            
-            if count(text) > 0 {
-                
-                self.userText = true
-                configureTextLabel(text)
-                
-            } else {
-                
-                configureTextLabel("100%")
-                
-            }
-            
-        }
-        
-        func configureTextLabel(string: String){
-            
-            label = UILabel(
-                frame: CGRectMake(0, 0, (self.frame.width * 1.35)/2,
-                    (self.frame.height * 0.9)/2))
-            
-            label.center = CGPoint(x: self.bounds.width/2, y: self.bounds.height/2)
-            label.textColor = UIColor.blackColor()
-            label.textAlignment = NSTextAlignment.Center
-            label.textColor = fillColor;
-            self.addSubview(label)
-            
-            var originalString: String = string
-            var fontSize : CGFloat = CGFloat(100)
-            let myString: NSString = originalString as NSString
-            var size: CGSize = myString.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(fontSize)])
-            
-            while(size.width > label.frame.width){
-                size = myString.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(--fontSize)])
-            }
-            
-            label.font = UIFont.systemFontOfSize(fontSize)
-            label.text = string
-        }
-        
-        required init(coder aDecoder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-        
-        
-        func configure(){
-            
-            var startAngle : CGFloat = CGFloat()
-            var endAngle   : CGFloat = CGFloat()
-            
-            switch startingPoint {
-                
-            case .Top:
-                startAngle = CGFloat(M_PI * -0.5)
-                endAngle   = CGFloat(M_PI * 1.5)
-                break
-                
-            case .Right:
-                startAngle = CGFloat(M_PI * 0.0)
-                endAngle   = CGFloat(M_PI * 2.0)
-                break
-                
-            case .Bottom:
-                startAngle = CGFloat(M_PI * 0.5)
-                endAngle   = CGFloat(M_PI * 2.5)
-                break
-                
-            case .Left:
-                startAngle = CGFloat(M_PI * 1)
-                endAngle   = CGFloat(M_PI * 3)
-                break
-                
-            default:
-                break
-                
-            }
-            
-            if !clockwise {
-                
-                let aux = startAngle
-                startAngle = endAngle
-                endAngle = aux
-            }
-            
-            var circlePath = UIBezierPath(arcCenter: CGPoint(x: self.bounds.size.width/2, y: self.bounds.height/2), radius: (frame.size.width - frame.size.width*0.05)/2, startAngle: startAngle, endAngle: endAngle , clockwise: clockwise)
-            
-            backgroundCircle.path = circlePath.CGPath
-            backgroundCircle.fillColor = UIColor.clearColor().CGColor
-            backgroundCircle.strokeColor = self.backgroundCircleColor.CGColor
-            backgroundCircle.lineWidth = frame.size.width * 0.05;
-            backgroundCircle.strokeEnd = 1.0;
-            layer.addSublayer(backgroundCircle)
-            
-            // Setup the CAShapeLayer with the path, colors, and line width
-            animatedCircle.path = circlePath.CGPath
-            animatedCircle.fillColor = UIColor.clearColor().CGColor
-            animatedCircle.strokeColor = fillColor.CGColor
-            animatedCircle.lineWidth = frame.size.width * 0.05;
-            
-            // Don't draw the circle initially
-            animatedCircle.strokeEnd = 0.0;
-            
-            // Add the circleLayer to the view's layer's sublayers
-            layer.addSublayer(animatedCircle)
-            
-            
-        }
-        
-        func animate(duration: NSTimeInterval) {
-            
-            if !userText {
-                timer = NSTimer.scheduledTimerWithTimeInterval(Double(Double(duration) / Double(percentage * 100)), target: self, selector: "numberIncrease", userInfo: nil, repeats: true)
-            }
-            
-            // We want to animate the strokeEnd property of the circleLayer
-            let animation = CABasicAnimation(keyPath: "strokeEnd")
-            
-            // Set the animation duration appropriately
-            animation.duration = duration
-            
-            // Animate from 0 (no circle) to 1 (full circle)
-            animation.fromValue = 0
-            animation.toValue = self.percentage
-            
-            // Do a linear animation (i.e. the speed of the animation stays the same)
-            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-            
-            // Set the circleLayer's strokeEnd property to 1.0 now so that it's the
-            // right value when the animation ends.
-            animatedCircle.strokeEnd = self.percentage
-            
-            // Do the actual animation
-            animatedCircle.addAnimation(animation, forKey: "animateCircle")
-        }
-        
-        
-        func numberIncrease(){
-            
-            if(self.auxNumber < percentage){
-                
-                self.auxNumber += 0.01;
-                label.text = "\(Int(self.auxNumber * 100))%"
-                
-            } else {
-                timer.invalidate()
-            }
-        }
+        case Top
+        case Right
+        case Bottom
+        case Left
         
     }
     
-    //Bonus, UIFont extension to calculate the size of a STRING
-    extension UIFont {
+    var startingPoint : StartingPoint = StartingPoint.Top
+    
+    private var backgroundCircle : CAShapeLayer = CAShapeLayer()
+    private let animatedCircle   : CAShapeLayer = CAShapeLayer()
+    
+    internal var backgroundCircleColor : UIColor = UIColor.grayColor()
+    internal var fillColor : UIColor = UIColor.blueColor()
+    
+    internal var clockwise : Bool = Bool(true)
+    internal var percentage : CGFloat = 1.0
+    private  var auxNumber : CGFloat = 0
+    
+    internal var label : UILabel = UILabel()
+    private var timer : NSTimer = NSTimer()
+    private var userText : Bool = Bool(false)
+    private var labelText : String = String("100%")
+    
+    override init(frame: CGRect){
         
-        func sizeOfString (string: NSString, constrainedToWidth width: Double) -> CGSize {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.clearColor()
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+        
+    }
+    
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setText(text: String){
+    
+        if count(text) > 0 {
             
-            return string.boundingRectWithSize(CGSize(width: width, height: DBL_MAX),
-                options: NSStringDrawingOptions.UsesLineFragmentOrigin,
-                attributes: [NSFontAttributeName: self],
-                context: nil).size
+            self.userText = true
+            labelText = text;
+
+        } else {
+            
+            self.userText = false
+            labelText = "100%";
+
         }
     
+        
+        
+    }
+    
+    func configure(){
+        
+        var startAngle : CGFloat = CGFloat()
+        var endAngle   : CGFloat = CGFloat()
+        
+        switch startingPoint {
+            
+        case .Top:
+            startAngle = CGFloat(M_PI * -0.5)
+            endAngle   = CGFloat(M_PI * 1.5)
+            break
+            
+        case .Right:
+            startAngle = CGFloat(M_PI * 0.0)
+            endAngle   = CGFloat(M_PI * 2.0)
+            break
+            
+        case .Bottom:
+            startAngle = CGFloat(M_PI * 0.5)
+            endAngle   = CGFloat(M_PI * 2.5)
+            break
+            
+        case .Left:
+            startAngle = CGFloat(M_PI * 1)
+            endAngle   = CGFloat(M_PI * 3)
+            break
+            
+        default:
+            break
+            
+        }
+        
+        if !clockwise {
+            
+            let aux = startAngle
+            startAngle = endAngle
+            endAngle = aux
+        }
+        
+        var circlePath = UIBezierPath(arcCenter: CGPoint(x: self.bounds.size.width/2, y: self.bounds.height/2), radius: (frame.size.width - frame.size.width*0.05)/2, startAngle: startAngle, endAngle: endAngle , clockwise: clockwise)
+        
+        backgroundCircle.path = circlePath.CGPath
+        backgroundCircle.fillColor = UIColor.clearColor().CGColor
+        backgroundCircle.strokeColor = self.backgroundCircleColor.CGColor
+        backgroundCircle.lineWidth = frame.size.width * 0.05;
+        backgroundCircle.strokeEnd = 1.0;
+        layer.addSublayer(backgroundCircle)
+        
+        // Setup the CAShapeLayer with the path, colors, and line width
+        animatedCircle.path = circlePath.CGPath
+        animatedCircle.fillColor = UIColor.clearColor().CGColor
+        animatedCircle.strokeColor = fillColor.CGColor
+        animatedCircle.lineWidth = frame.size.width * 0.05;
+        
+        // Don't draw the circle initially
+        animatedCircle.strokeEnd = 0.0;
+        
+        // Add the circleLayer to the view's layer's sublayers
+        layer.addSublayer(animatedCircle)
+        
+        configureTextLabel(labelText)
+    }
+    
+    func configureTextLabel(string: String){
+        
+        label = UILabel(
+            frame: CGRectMake(0, 0, (self.frame.width * 1.35)/2,
+                (self.frame.height * 0.9)/2))
+        
+        label.center = CGPoint(x: self.bounds.width/2, y: self.bounds.height/2)
+        label.textColor = UIColor.blackColor()
+        label.textAlignment = NSTextAlignment.Center
+        label.textColor = fillColor;
+        self.addSubview(label)
+        
+        var originalString: String = string
+        var fontSize : CGFloat = CGFloat(100)
+        let myString: NSString = originalString as NSString
+        var size: CGSize = myString.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(fontSize)])
+        
+        while(size.width > label.frame.width){
+            size = myString.sizeWithAttributes([NSFontAttributeName: UIFont.systemFontOfSize(--fontSize)])
+        }
+        
+        label.font = UIFont.systemFontOfSize(fontSize)
+        
+        if userText {
+            
+            label.text = string
 
+        }
+    }
+    
+    
+    func animate(duration: NSTimeInterval) {
+        
+        if !userText {
+            timer = NSTimer.scheduledTimerWithTimeInterval(Double(Double(duration) / Double(percentage * 100)), target: self, selector: "numberIncrease", userInfo: nil, repeats: true)
+        }
+        
+        // We want to animate the strokeEnd property of the circleLayer
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        // Set the animation duration appropriately
+        animation.duration = duration
+        
+        // Animate from 0 (no circle) to 1 (full circle)
+        animation.fromValue = 0
+        animation.toValue = self.percentage
+        
+        // Do a linear animation (i.e. the speed of the animation stays the same)
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        
+        // Set the circleLayer's strokeEnd property to 1.0 now so that it's the
+        // right value when the animation ends.
+        animatedCircle.strokeEnd = self.percentage
+        
+        // Do the actual animation
+        animatedCircle.addAnimation(animation, forKey: "animateCircle")
+    }
+    
+    
+    func numberIncrease(){
+        
+        if(self.auxNumber < percentage){
+            
+            self.auxNumber += 0.01;
+            label.text = "\(Int(self.auxNumber * 100))%"
+            
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+}
+
+//Bonus, UIFont extension to calculate the size of a STRING
+extension UIFont {
+    
+    func sizeOfString (string: NSString, constrainedToWidth width: Double) -> CGSize {
+        
+        return string.boundingRectWithSize(CGSize(width: width, height: DBL_MAX),
+            options: NSStringDrawingOptions.UsesLineFragmentOrigin,
+            attributes: [NSFontAttributeName: self],
+            context: nil).size
+    }
+    
+    
 }
--- CAView/ViewController.swift
+++ CAView/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        var button = UIButton(frame: CGRectMake(50, 175, 100, 50))
+        button.addTarget(self, action: "simpleView", forControlEvents: UIControlEvents.TouchUpInside)
+        button.setTitle("Simple View", forState: UIControlState.Normal)
+        button.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(button)
+        
+        //circle that will Animate Counter ClockWise starting from the far left and animating only until 17%
+        
+        var button2 = UIButton(frame: CGRectMake(50, 350, 100, 50))
+        button2.addTarget(self, action: "exampleTwo", forControlEvents: UIControlEvents.TouchUpInside)
+        button2.setTitle("Example 2", forState: UIControlState.Normal)
+        button2.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(button2)
+        
+        
+        var button3 = UIButton(frame: CGRectMake(200, 175, 100, 50))
+        button3.addTarget(self, action: "exampleThree", forControlEvents: UIControlEvents.TouchUpInside)
+        button3.setTitle("Example 3", forState: UIControlState.Normal)
+        button3.backgroundColor = UIColor.grayColor()
+        self.view.addSubview(button3)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +43,60 @@ class ViewController: UIViewController {
     }
 
 
+    func simpleView(){
+    
+        self.view.viewWithTag(200)?.removeFromSuperview()
+        
+        let circle = CAView(frame: CGRectMake(50, 50, 100, 100))
+        circle.configure()
+        circle.tag = 200
+        self.view.addSubview(circle)
+        
+        circle.animate(2.0)
+    
+    }
+    
+    //circle that will Animate Counter ClockWise starting from the far left and animating only until 17%
+
+    func exampleTwo(){
+        
+        self.view.viewWithTag(201)?.removeFromSuperview()
+
+        let circle = CAView(frame: CGRectMake(50, 245, 100, 100))
+        circle.percentage = 0.17
+        circle.clockwise = false
+        circle.startingPoint = CAView.StartingPoint.Left
+        circle.backgroundColor = UIColor.blackColor()
+        circle.backgroundCircleColor = UIColor.yellowColor()
+        circle.fillColor = UIColor.redColor()
+        circle.configure()
+        self.view.addSubview(circle)
+        
+        circle.tag = 201
+
+        
+        circle.animate(3.0)
+    }
+    
+    
+    func exampleThree(){
+        
+        self.view.viewWithTag(201)?.removeFromSuperview()
+        
+        let circle = CAView(frame: CGRectMake(200, 50, 100, 100))
+        circle.setText("My Text")
+        circle.backgroundCircleColor = UIColor.blackColor()
+        circle.configure()
+        circle.percentage = 0.5
+        circle.startingPoint = CAView.StartingPoint.Bottom
+        circle.fillColor = UIColor.yellowColor()
+        circle.label.textColor = UIColor.redColor()
+        self.view.addSubview(circle)
+        
+        circle.animate(0.2)
+        circle.tag = 201
+        
+        
+    }
 }
 
